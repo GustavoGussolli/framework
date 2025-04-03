71c6720456456801e22d48a225d20c132a612fa5
GustavoGussolli <134729255+GustavoGussolli@users.noreply.github.com>
2025-04-03 15:43:34 -0300

    Add files via upload

--- framework/aula-02/funcoes.js
+++ framework/aula-02/funcoes.js
@@ -1,78 +1,113 @@
 var ctxCabecalho;
 var ctxLinks;
-var ctxConteudo;
-var ctxRodape;
 
-function configEstiloCabecalho(){
+function configEstiloCabecalho() {
     let bg = document.getElementById("corFundo").value;
     let corFonte = document.getElementById("corFonte").value;
     let tamFonte = document.getElementById("tamFonte").value;
-    ctxCabecalho = "#cabecalho{\n background-color:"+bg+";\n";
-    ctxCabecalho += " color:"+corFonte+";\n";
-    ctxCabecalho += " font-size:"+tamFonte+"pt;\n}\n";
+    
+    ctxCabecalho = `#cabecalho {
+        background-color: ${bg};
+        color: ${corFonte};
+        font-size: ${tamFonte}pt;
+    }\n`;
+    
     return ctxCabecalho;
 }
 
-function configEstiloLinks(){
+function configEstiloLinks() {
     let corLink = document.getElementById("corLinks").value;
-    let estiloLink = document.querySelector('input[name="estiloLinks"]:checked').value;
-    ctxLinks = "a{\n color:"+corLink+";\n";
-    let aux = estiloLink == "0" ? "none" : "underline";
-    ctxLinks += " text-decoration:"+aux+";\n}\n";
+    let estiloLink = document.querySelector('input[name="estiloLinks"]:checked');
+    
+    let aux = (estiloLink && estiloLink.value === "0") ? "none" : "underline";
+    
+    ctxLinks = `a {
+        color: ${corLink};
+        text-decoration: ${aux};
+    }\n`;
+    
     return ctxLinks;
 }
 
-function configHTMLLinks(){
-
-    links=document.querySelector("#links").value;
-    vetLinks=links.split(";");
-    ctxLinks="";
-
-    for(let i=0; i<vetLinks.length;i++){
-
-        ctxLinks += '<a href="#">'+vetLinks[i]+'</a>';
+function configHTMLLinks() {
+    let linkInputs = document.getElementsByName("links");
+    let hrefInputs = document.getElementsByName("href");
+    
+    ctxLinks = "";
+    
+    for (let i = 0; i < linkInputs.length; i++) {
+        let href = hrefInputs[i].value.split("\\").pop(); // Obtém o nome do arquivo
+        ctxLinks += `<a href="${href}">${linkInputs[i].value}</a>\n`;
     }
 
     return ctxLinks;
-    
 }
 
-function configHTMLCabecalho(){
+function configHTMLCabecalho() {
     let aux = document.querySelector("#textoCabecalho").value;
-    ctxCabecalho = '<h1>'+aux+'</h1>'
-    return ctxCabecalho;
-
+    return `<h1>${aux}</h1>`;
 }
 
-function gerarCodigo(){
-    //Código para CSS
+function gerarCodigo() {
+    // Geração do código CSS
     let codeCSS = document.querySelector("#codeCSS");
     let css = configEstiloCabecalho();
     css += configEstiloLinks();
     codeCSS.value = css;
 
-    //Código para HTML
+    // Geração do código HTML
     let codeHTML = document.querySelector("#codeHTML");
-    let ctxHTML = "<html>\n<head>\n" +
-    "<link rel='Stylesheet' href='estilo.css'>\n" +
-    "<title>Minha Página</title>\n" +
-    "</head>\n<body>\n" +
-    "<div id='cabecalho'> " + configHTMLCabecalho() + " </div>\n" +
-    "<nav id='links'>\n" + configHTMLLinks() + "\n</nav>\n" +
-    "<div id='conteudo'></div>\n" +
-    "</body>\n</html>";
+    let ctxHTML = `<!DOCTYPE html>
+<html>
+<head>
+    <link rel='stylesheet' href='estilo.css'>
+    <title>Minha Página</title>
+</head>
+<body>
+    <div id='cabecalho'>${configHTMLCabecalho()}</div>
+    <nav id='links'>${configHTMLLinks()}</nav>
+    <div id='conteudo'></div>
+</body>
+</html>`;
+    
     codeHTML.value = ctxHTML;
 }
 
-function download(campo, arquivo){
+function download(campo, arquivo) {
+    if (arquivo.trim() === '')
+        arquivo = document.getElementById("nomeHTML").value + ".html";
 
     var text = document.getElementById(campo).value;
-    var blob = new Blob([text], {type: "text/plain"});
+    var blob = new Blob([text], { type: "text/plain" });
     var a = document.createElement("a");
+    
     a.href = URL.createObjectURL(blob);
-    a.download = arquivo;
+    a.download = arquivo.trim();
+    
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
-
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("gerarMais").addEventListener("click", function () {
+        let div = document.createElement("div");
+        div.classList.add("mb-2");
+
+        let inputTexto = document.createElement("input");
+        inputTexto.type = "text";
+        inputTexto.name = "links";
+        inputTexto.classList.add("form-control", "mb-2");
+        inputTexto.placeholder = "Texto do link";
+
+        let inputFile = document.createElement("input");
+        inputFile.type = "file";
+        inputFile.name = "href";
+        inputFile.classList.add("form-control");
+
+        div.appendChild(inputTexto);
+        div.appendChild(inputFile);
+
+        document.getElementById("areaLinks").appendChild(div);
+    });
+});
